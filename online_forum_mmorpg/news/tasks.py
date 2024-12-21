6a794a9cbf91234f37ac673931b5cc8aceb715b9
Svetlana Krasnova <venerliline@yandex.ru>
2024-12-21 12:42:12 +0300

    sending notifications refactoring code

--- online_forum_mmorpg/news/tasks.py
+++ online_forum_mmorpg/news/tasks.py
@@ -1,22 +1,20 @@
+import redis
+import logging
+import json
 from datetime import datetime, timedelta
 from celery import shared_task
 from news.models import News
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
-from django.core.mail import EmailMultiAlternatives
 from django.conf import settings
+from online_forum_mmorpg.utils import send_email_notification
+
+logger = logging.getLogger(__name__)
 
 
 @shared_task
-def sent_email_task(message: dict):
-    msg = EmailMultiAlternatives(
-        subject=message["subject"],
-        body="",
-        from_email=message["from_email"],
-        to=message["to"],
-    )
-    msg.attach_alternative(message["html_content"], "text/html")
-    msg.send()
+def send_email_task(message: dict):
+    send_email_notification(message)
 
 
 @shared_task
@@ -32,17 +30,15 @@ def notify_week_posts() -> None:
     if not news:
         return
 
-    msg_html = render_to_string('notify_week_news_mail.html', {
+    msg_html = render_to_string('news/notify_week_news_mail.html', {
         'link_home': settings.SITE_URL,
         'link_filter': f'{settings.SITE_URL}/news/search_news/?title=&create_date__gt={start_date.strftime("%Y-%m-%d")}',
         'news': news,
     })
 
     for email in users_emails:
-        sent_email_task.apply_async([{
+        send_email_task.apply_async([{
             "subject": "Новости за неделю",
             "html_content": msg_html,
-            "from_email": settings.DEFAULT_FROM_EMAIL,
             "to": [email]
-
         }], countdown=1)
