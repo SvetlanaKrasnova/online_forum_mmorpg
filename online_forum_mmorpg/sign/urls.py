d35876ed92f966eb110fc1377df5315a74ef1b43
Svetlana Krasnova <venerliline@yandex.ru>
2024-12-22 18:26:12 +0300

    refactoring code

--- online_forum_mmorpg/sign/urls.py
+++ online_forum_mmorpg/sign/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import Profile
+from .views import ProfileView
 
 urlpatterns = [
-    path('profile', Profile.as_view(), name='profile'),
-    path('profile/reactions/search/', Profile.as_view(), name='reactions_search'),
+    path('profile', ProfileView.as_view(), name='profile'),
+    path('profile/reactions/search/', ProfileView.as_view(), name='reactions_search'),
 ]
